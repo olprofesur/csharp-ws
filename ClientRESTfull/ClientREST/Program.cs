8d1ea06122b61b73659d90b37cd074a039d2005a
olprofesur <diego.bernini@gmail.com>
2023-01-18 07:56:03 +0100

    added full REST sample client

--- ClientRESTfull/ClientREST/Program.cs
+++ ClientRESTfull/ClientREST/Program.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -44,6 +45,35 @@ namespace HttpClientSample
             return product;
         }
 
+        static async Task<Uri> CreateProductAsync(Album product)
+        {
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "/albums", product);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<Album> UpdateProductAsync(Album product)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"/albums/{product.id}", product);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            product = await JsonSerializer.DeserializeAsync<Album>(await response.Content.ReadAsStreamAsync());
+            return product;
+        }
+
+        static async Task<HttpStatusCode> DeleteProductAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"/albums/{id}");
+            return response.StatusCode;
+        }
+
 
         static void Main()
         {
@@ -64,6 +94,24 @@ namespace HttpClientSample
                 // Get an existing product
                 album = await GetAlbumAsync("/albums/1");
                 ShowProduct(album);
+
+                //Create a new product
+                album = new Album();
+                album.id = 99; album.title = "Test"; album.userId = 1;
+                var url = await CreateProductAsync(album);
+                Console.WriteLine($"New album reated at {url}");
+                /*
+                album = await GetAlbumAsync(url);
+                ShowProduct(album);*/
+
+                // Update the product
+                Console.WriteLine("Updating title...");
+                album.title = "Test2";
+                await UpdateProductAsync(album);
+
+                // Delete the product
+                var statusCode = await DeleteProductAsync(album.id + "");
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
 
             }
             catch (Exception e)
