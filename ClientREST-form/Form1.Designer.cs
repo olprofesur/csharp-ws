717b13ab122899d842fed10c852141ca9a8c359e
olprofesur <diego.bernini@gmail.com>
2023-01-26 16:49:55 +0100

    added base for client form-based

---
+++ ClientREST-form/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ClientREST_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelUserID = new System.Windows.Forms.Label();
+            this.textBoxUserID = new System.Windows.Forms.TextBox();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(101, 34);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(100, 28);
+            this.textBoxID.TabIndex = 0;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(101, 88);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(335, 28);
+            this.textBoxTitle.TabIndex = 1;
+            this.textBoxTitle.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(59, 36);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(27, 22);
+            this.labelID.TabIndex = 2;
+            this.labelID.Text = "ID";
+            this.labelID.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(41, 91);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(45, 22);
+            this.labelTitle.TabIndex = 3;
+            this.labelTitle.Text = "Title";
+            this.labelTitle.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // labelUserID
+            // 
+            this.labelUserID.AutoSize = true;
+            this.labelUserID.Location = new System.Drawing.Point(21, 144);
+            this.labelUserID.Name = "labelUserID";
+            this.labelUserID.Size = new System.Drawing.Size(65, 22);
+            this.labelUserID.TabIndex = 5;
+            this.labelUserID.Text = "UserID";
+            this.labelUserID.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // textBoxUserID
+            // 
+            this.textBoxUserID.Location = new System.Drawing.Point(101, 144);
+            this.textBoxUserID.Name = "textBoxUserID";
+            this.textBoxUserID.Size = new System.Drawing.Size(335, 28);
+            this.textBoxUserID.TabIndex = 4;
+            this.textBoxUserID.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(160, 217);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(188, 35);
+            this.buttonRead.TabIndex = 6;
+            this.buttonRead.Text = "READ";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 299);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.labelUserID);
+            this.Controls.Add(this.textBoxUserID);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.textBoxID);
+            this.Name = "Form1";
+            this.Text = "Sample Client REST";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelUserID;
+        private System.Windows.Forms.TextBox textBoxUserID;
+        private System.Windows.Forms.Button buttonRead;
+    }
+}
+
---
+++ ClientREST-form/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientREST_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ClientREST.init();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string id = textBoxID.Text;
+            int idInt;
+            Album a=null;
+            if (id != null && int.TryParse(id, out idInt))
+            {
+                a=await ClientREST.GetAlbumAsync("/albums/" + id);
+                textBoxTitle.Text = a.title;
+                textBoxUserID.Text = a.userId+"";
+            }
+            
+
+        }
+    }
+
+    public class Album
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+
+        override
+        public string ToString()
+        {
+            return "Album: " + id + " " + title + " owned by " + userId;
+        }
+    }
+
+
+    class ClientREST
+    {
+
+        static HttpClient client = new HttpClient();
+
+        public static void init()
+        {
+            client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+
+        static void ShowProduct(Album album)
+        {
+            Console.WriteLine(album.ToString());
+        }
+
+        public static async Task<Album> GetAlbumAsync(string path)
+        {
+            Album product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await JsonSerializer.DeserializeAsync<Album>(await response.Content.ReadAsStreamAsync());
+            }
+            return product;
+        }
+
+        static async Task<Uri> CreateProductAsync(Album product)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "/albums", product);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<Album> UpdateProductAsync(Album product)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"/albums/{product.id}", product);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            product = await JsonSerializer.DeserializeAsync<Album>(await response.Content.ReadAsStreamAsync());
+            return product;
+        }
+
+        static async Task<HttpStatusCode> DeleteProductAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"/albums/{id}");
+            return response.StatusCode;
+        }
+
+
+        /*static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }*/
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                Album album = null;
+
+                // Get an existing product
+                album = await GetAlbumAsync("/albums/1");
+                ShowProduct(album);
+
+                //Create a new product
+                album = new Album();
+                album.id = 99; album.title = "Test"; album.userId = 1;
+                var url = await CreateProductAsync(album);
+                Console.WriteLine($"New album reated at {url}");
+                /*
+                album = await GetAlbumAsync(url);
+                ShowProduct(album);*/
+
+                // Update the product
+                Console.WriteLine("Updating title...");
+                album.title = "Test2";
+                await UpdateProductAsync(album);
+
+                // Delete the product
+                var statusCode = await DeleteProductAsync(album.id + "");
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
